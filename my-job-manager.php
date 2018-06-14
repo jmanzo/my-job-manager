236d4286919eea41500fb53337823b5862d854a6
Jean Manzo <jmanzosistemas@gmail.com>
2018-06-14 12:06:09 -0500

    first commit

---
+++ my-job-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package My Job Manager
+ * @version 1.0
+ */
+/*
+Plugin Name: My Job Manager
+Plugin URI: https://github.com/jmanzo/my-job-manager
+Description: This is a job manager plugin for Wordpress sites.
+Author: Wordpress Medium Community
+Version: 1.0
+Author URI: http://about.me/jeanmanzo
+*/
